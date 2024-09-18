53a5ad7d4eb700196087484365011809cca7e30e
= <thamizharaasan51@gmail.com>
2024-09-18 20:33:47 +0800

    passed

--- leetcode-1/Solution.java
+++ leetcode-1/Solution.java
@@ -1,17 +1,29 @@
-import java.util.*;
+import java.util.*; 
 
 public class Solution{
 
     public static int[] twoSum(int[] nums, int target) {
-        return nums;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = (target - nums[i]);
+            if (map.containsKey(diff)){
+                return new int[]{map.get(diff), i};
+            }
+            else if (!map.containsKey(diff)){
+                map.put(nums[i], i);
+            }
+                
+        }
+        return null;
         
     }
 
 
-    public static void main(String[] args) {
-        int[] nums = {2,7,11,15};
-        int target = 9;
+    // TESTING
+    // public static void main(String[] args) {
+    //     int[] nums = {3,3};
+    //     int target = 6;
 
-        System.out.println(Arrays.toString(twoSum(nums, target)));
-    }
+    //     System.out.println(Arrays.toString(twoSum(nums, target)));
+    // }
 }
