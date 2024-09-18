4d2d3fcaaf579f5342a268c1632ded185d9da3a5
= <thamizharaasan51@gmail.com>
2024-09-18 20:38:59 +0800

    test

---
+++ leetcode-2/Solution.java
@@ -0,0 +1,19 @@
+import java.util.*; 
+
+public class Solution{
+
+    public static int[] twoSum(int[] nums, int target) {
+        return nums;
+
+        
+    }
+
+
+    // TESTING
+    public static void main(String[] args) {
+        int[] nums = {3,3};
+        int target = 6;
+
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+}
